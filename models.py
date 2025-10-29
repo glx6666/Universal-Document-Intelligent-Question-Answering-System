c33f6faafe532985e028d33b433d2390636ace16
ZMX <148468727+glx6666@users.noreply.github.com>
2025-10-29 21:56:34 +0800

    Update models.py

--- models.py
+++ models.py
@@ -5,14 +5,14 @@ from langchain_openai import ChatOpenAI
 import inspect
 from langchain_community.embeddings import DashScopeEmbeddings
 
-ALI_TONGYI_API_KEY_OS_VAR_NAME = "DASHSCOPE_API_KEY"
-ALI_TONGYI_URL = "https://dashscope.aliyuncs.com/compatible-mode/v1"
-ALI_TONGYI_MAX_MODEL = "qwen-max-latest"
-ALI_TONGYI_DEEPSEEK_R1 = "deepseek-r1"
-ALI_TONGYI_DEEPSEEK_V3 = "deepseek-v3"
-ALI_TONGYI_REASONER_MODEL = "qvq-max-latest"
-ALI_TONGYI_EMBEDDING_MODEL = "text-embedding-v3"
-ALI_TONGYI_RERANK_MODEL = "gte-rerank-v2"
+ALI_TONGYI_API_KEY_OS_VAR_NAME = ""
+ALI_TONGYI_URL = "/compatible-mode/v1"
+ALI_TONGYI_MAX_MODEL = ""
+ALI_TONGYI_DEEPSEEK_R1 = ""
+ALI_TONGYI_DEEPSEEK_V3 = ""
+ALI_TONGYI_REASONER_MODEL = ""
+ALI_TONGYI_EMBEDDING_MODEL = ""
+ALI_TONGYI_RERANK_MODEL = ""
 
 def get_lc_model_client(api_key=os.getenv(ALI_TONGYI_API_KEY_OS_VAR_NAME), base_url=ALI_TONGYI_URL
                         , model=ALI_TONGYI_MAX_MODEL, temperature=0.7, max_tokens=8000,verbose=False, debug=False):
@@ -43,5 +43,6 @@ def get_ali_rerank(top_n=10):
         model=ALI_TONGYI_RERANK_MODEL, dashscope_api_key=os.getenv(ALI_TONGYI_API_KEY_OS_VAR_NAME),
         top_n=top_n
 )
+
 
 
